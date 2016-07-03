24d675cd849fb928e48534fd3f18a0fd8d7cca47
altokumulus <praveen@altokumulus.com>
2016-07-03 10:22:48 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
-var app = express();
+var app = require('express');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
