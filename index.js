403b552e981c05d28fa05dbc849673c0e6189283
altokumulus <praveen@altokumulus.com>
2016-07-03 13:14:08 -0700

    Update index.js

--- index.js
+++ index.js
@@ -23,6 +23,44 @@ const wit = new Wit({
   logger: new log.Logger(log.INFO)
 });
 
+//wit actions
+const actions = {
+  send(request, response) {
+    const {sessionId, context, entities} = request;
+    const {text, quickreplies} = response;
+    return new Promise(function(resolve, reject) {
+      console.log('sending...', JSON.stringify(response));
+      return resolve();
+    });
+  },
+  getForecast({context, entities}) {
+    return new Promise(function(resolve, reject) {
+      var location = firstEntityValue(entities, 'location')
+      if (location) {
+        context.forecast = 'sunny in ' + location; // we should call a weather API here
+        delete context.missingLocation;
+      } else {
+        context.missingLocation = true;
+        delete context.forecast;
+      }
+      return resolve(context);
+    });
+  },
+};
+
+const firstEntityValue = (entities, entity) => {
+  const val = entities && entities[entity] &&
+    Array.isArray(entities[entity]) &&
+    entities[entity].length > 0 &&
+    entities[entity][0].value
+  ;
+  if (!val) {
+    return null;
+  }
+  return typeof val === 'object' ? val.value : val;
+};
+//end wit code
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
