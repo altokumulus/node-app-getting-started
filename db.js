2a279ad9959a2088bfd45305bd9334c097144f40
altokumulus <praveen@altokumulus.com>
2016-07-09 17:38:02 -0700

    Update db.js

--- db.js
+++ db.js
@@ -12,7 +12,24 @@ var pool      =    mysql.createPool({
     debug    :  false
 });
 module.exports = {
-function handle_database(req,res) {
+    handle_database: function(req, res) {
+    pool.getConnection(function(err,connection){
+        if (err) {
+          res.json({"code" : 100, "status" : "Error in connection database"});
+          return;
+        }   
+
+        console.log('connected as id ' + connection.threadId);
+        
+    
+
+        connection.on('error', function(err) {      
+              res.json({"code" : 100, "status" : "Error in connection database"});
+              return;     
+        });
+  });
+  },
+/*function handle_database(req,res) {
     
     pool.getConnection(function(err,connection){
         if (err) {
@@ -29,11 +46,12 @@ function handle_database(req,res) {
               return;     
         });
   });
-}
+}*/
 
 //app.get("/",function(req,res){-
   //      handle_database(req,res);
-});
-};
+}
+//);
+//};
 
 //app.listen(3000);
